6c9594d8b109519304b78e22f2b6d3d5979eb9eb
Sara Olsson <sara.olsson@gaia.se>
2021-04-25 17:11:22 +0200

    Extract item values

--- quickstart-react/src/components/TaskItem.js
+++ quickstart-react/src/components/TaskItem.js
@@ -13,9 +13,21 @@ const TaskItem = (props) => {
             <>
             {
                 props.itemData.map(item => {
+                    let column_values = item.column_values
+                    //console.log(column_values)
+                    let status = column_values.find(c => c.title === "Status")
+                    let person = column_values.find(c => c.title === "Person")
+                    let details = column_values.find(c => c.title === "Details")
+                     
+
                     return (
                     <div style={{margin: 10, padding: 10, backgroundColor: 'gray', borderRadius: 5}}> 
                         <p>Name: {item.name}</p>
+                        { status && <p>Status: {status.text}</p> }
+                        { person && <p>Person: {person.text}</p> }
+                        { details && <p>Details: {details.text}</p> }
+
+                        <p> {JSON.stringify(column_values)} </p>
                     </div>
                     )
                 })
